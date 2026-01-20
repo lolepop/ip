06ad3860e97711a1a736cc74db6cfdb2d6ea08ec
lolepop <8401103+lolepop@users.noreply.github.com>
2026-01-20 22:52:35 +0800

    complete echo task

--- src/main/java/Dawg.java
+++ src/main/java/Dawg.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Dawg {
+
     public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+
         System.out.println("Hello, I'm Dawg\nWhat can I do for you?");
         
+        while (true) {
+            String userCommand = stdin.nextLine();
+            if (userCommand.equals("bye")) {
+                break;
+            } else {
+                System.out.println(userCommand + "\n");
+            }
+        }
         
         System.out.println("Bye. Hope to see you again soon!");
+        stdin.close();
     }
 }
