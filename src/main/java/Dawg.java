39af5319915f97e38c9533d569f486da231c40cf
lolepop <8401103+lolepop@users.noreply.github.com>
2026-01-20 22:44:33 +0800

    setup project

---
+++ src/main/java/Dawg.java
@@ -0,0 +1,8 @@
+public class Dawg {
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm Dawg\nWhat can I do for you?");
+        
+        
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
